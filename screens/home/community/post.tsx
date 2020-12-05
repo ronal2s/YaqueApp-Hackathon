659379c5c3e1c07df3998a203327ed3ce9d8b2f8
Renys De La Cruz <ronal2w@gmail.com>
2020-12-05 16:39:32 -0400

    showing up admin comments

--- screens/home/community/post.tsx
+++ screens/home/community/post.tsx
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Card, Button } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
+import MapView from 'react-native-maps';
 //Custom Components
 import CustomButton from "../../../components/button";
 //Utils
 import { ScrollView, Text, View } from "../../../styles/emotions";
-import { COLORS } from "../../../utils/enums";
+import { COLORS, Screens } from "../../../utils/enums";
 import { IFPost } from "../../../utils/globalInterfaces";
 //Modals
 import ModalComment from "./modalComment";
 //Service
 import * as FirestoreService from "../../../services/firestore";
 import { GlobalContext } from "../../../contexts/globalContexts";
-import { Linking } from "react-native";
+import { Dimensions, Linking } from "react-native";
 
 interface IViewPost {
     // post: IFPost
@@ -21,6 +22,7 @@ interface IViewPost {
 function ViewPost(props: IViewPost) {
     const post: IFPost = props.route.params.post;
     const allPosts: IFPost[] = props.route.params.allPosts;
+    const navigation = props.navigation;
     const [modalComment, setModalComment] = useState(false);
     const [currentPost, setCurrentPost] = useState<IFPost>(post);
     const [loading, setLoading] = useState(false);
@@ -57,6 +59,8 @@ function ViewPost(props: IViewPost) {
         } catch (error) {
             globalContext.setAlert("Error", error.message, true);
         }
+        
+        navigation.goBack();
         setLoading(false);
     }
 
@@ -73,20 +77,49 @@ function ViewPost(props: IViewPost) {
                     <Card containerStyle={{ margin: 0 }}>
                         <Card.FeaturedTitle style={{ color: "black" }} >{currentPost.title}</Card.FeaturedTitle>
                         {currentPost.picture !== "" && <Card.Image source={{ uri: currentPost.picture }} />}
-                        {currentPost.region.latitude && <Button title="Ver ubicación" type="outline" onPress={goToLocation} />}
+                        {currentPost.region.latitude &&
+                            <View flex={1} height={200} >
+                                <MapView pitchEnabled={false} rotateEnabled={false} zoomEnabled={false} scrollEnabled={false}
+                                    style={{ flex: 1, width: "100%" }}
+                                    region={currentPost.region}
+                                // showsUserLocation={true}
+                                >
+                                </MapView>
+                                <View style={{
+                                    left: '48.8%',
+                                    position: 'absolute',
+                                    top: '25%'
+                                }}>
+                                    <Icon name="map-marker-alt" type="font-awesome-5" color={COLORS.PRIMARY} size={40} />
+                                </View>
+                            </View>}
+                        {currentPost.region.latitude && <Button title="Ir a ubicación" type="outline" onPress={goToLocation} />}
                         <Text>{currentPost.description}</Text>
                     </Card>
-                    {currentPost.comments.map((comment, key) => {
-                        return (
-                            <Card containerStyle={{ margin: 0 }} key={key} >
-                                <Text>
-                                    <Text fontWeight="bold" color={COLORS.SECONDARY} >{comment.name} </Text>
-                                    comenta:
-                                </Text>
-                                <Text>{comment.text}</Text>
-                            </Card>
-                        )
-                    })}
+                    
+                        {currentPost.comments.map((comment, key) => {
+                            return (
+                                <Card containerStyle={{ margin: 0 }} key={key} >
+                                    <View flexDirection="column" >
+                                        {comment.verified && <View>
+                                            <Icon name="certificate" type="font-awesome-5" color={COLORS.PRIMARY} />
+                                            <Text fontWeight="bold" color={COLORS.PRIMARY} textAlign="center" >Administrador</Text>
+                                        </View>}
+                                        <View>
+                                            {!comment.verified &&
+                                                <Text>
+                                                    <Text fontWeight="bold" color={COLORS.SECONDARY} >{comment.name} </Text>
+                                            comenta:
+                                            </Text>
+                                            }
+                                            <Text textAlign={comment.verified ? "center" : "left"} >{comment.text}</Text>
+                                        </View>
+
+                                    </View>
+                                </Card>
+                            )
+                        })}
+                    
                 </ScrollView>
             </View>
             <CustomButton loading={false} noBorder title="Comentar" onPress={openComment} />
